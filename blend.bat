13b40be946d40dcc4be1f2a8d3c1bb06f71ea947
Martin P <43763327+cozmobotics@users.noreply.github.com>
2021-05-01 19:14:45 +0200

    Create blend.bat

---
+++ blend.bat
@@ -0,0 +1,5 @@
+:: Windwos batch file to envoke blending_padding
+:: Put (a link to) this batchfile onto your desktop 
+:: drag a folder from your file manager over the icon to invoke the program
+
+python3 your\path\to\program\blending_padding.py -p %1 -l -1 -s 1
