f926d50997a6ba802537de8516adfe5747b2dfe2
Martin P <43763327+cozmobotics@users.noreply.github.com>
2021-05-21 10:35:44 +0200

    Updated filename, added compiled version

--- blend.bat
+++ blend.bat
@@ -1,5 +1,9 @@
-:: Windwos batch file to envoke blending_padding
+:: Windwos batch file to envoke blendPics
 :: Put (a link to) this batchfile onto your desktop 
 :: drag a folder from your file manager over the icon to invoke the program
+:: Edit this file to meet your preferences. 
 
-python3 your\path\to\program\blending_padding.py -p %1 -l -1 -s 1
+:: from source code or from compiled file. Uncomment one of the following lines: 
+:: python3 your\path\to\program\blendPics.py -p %1 -l -1 -s 1
+:: python3 your\path\to\program\blendPics.py -p %1 -l -1 -s 1
+your\path\to\program\blendPics.exe -p %1 -l -1 -s 1
